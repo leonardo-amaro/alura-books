0fafd4b76569d08c11fefbe6ea654edc046a49f8
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-28 11:20:38 -0300

    corrigindo estilos da página e do Logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,10 +1,13 @@
+import './Logo.css'
 import logo from '../../images/logo.svg'
 
 const Logo = () => {
   return (
     <div className="logo">
       <img src={logo} alt="Logo Alura Books" />
-      <p>Alura Books</p>
+      <p>
+        <strong>Alura</strong>Books
+      </p>
     </div>
   )
 }
