92601e7d632c7284d94d370f5bef84f7ee988778
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-28 11:51:19 -0300

    adicionando e estilizando itens do Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
 import Logo from './components/Logo'
+import perfil from './images/perfil.svg'
+import sacola from './images/sacola.svg'
+
+const itensHeader = ['Categorias', 'Favoritos', 'Minha estante']
+const iconesHeader = [perfil, sacola]
 
 const App = () => {
   return (
     <div className='App'>
       <header className="App-header">
         <Logo />
+        <ul>
+          {itensHeader.map(
+            (item, index) => <li key={index}>{item}</li>
+          )}
+        </ul>
+        <ul>
+          {iconesHeader.map(
+            (icone, index) => (
+              <li key={index}>
+                <img src={icone} alt="Icone" />
+              </li>
+            )
+          )}
+        </ul>
       </header>
     </div>
   )
