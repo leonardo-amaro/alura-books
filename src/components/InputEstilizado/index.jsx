11cd6a3af76c5864292e3a43abf3b2ebc62c6d9b
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-31 11:23:19 -0300

    criando Input Estilizado

---
+++ src/components/InputEstilizado/index.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const InputEstilizado = styled.input`
+  border: 2px solid #FFF;
+  border-radius: 8px;
+  background: transparent;
+  margin: 32px 0;
+  padding: 16px 8px;
+  min-width: 80%;
+  color: #FFF;
+  font-size: 16px;
+`
+
+export default InputEstilizado
