efcb2a4ea1c073e5f863fac16eabcb6abae2a097
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2025-03-31 00:41:08 +0000

    removendo CSS para usar apenas styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import styled from 'styled-components'
 import EstilosGlobais from './components/EstilosGlobais'
 import Logo from './components/Logo'
 import perfil from './images/perfil.svg'
@@ -7,11 +7,33 @@ import sacola from './images/sacola.svg'
 const itensHeader = ['Categorias', 'Favoritos', 'Minha estante']
 const iconesHeader = [perfil, sacola]
 
+const AppContainer = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  background-image: linear-gradient(90deg, #002F52 35%, #326589);
+`
+const HeaderEstilizado = styled.header`
+  background-color: #f1f1f1;
+  padding: 32px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ul {
+    display: flex;
+    gap: 16px;
+  }
+  ul li {
+    list-style-type: none;
+    cursor: pointer;
+    font-weight: 500;
+  }
+`
+
 const App = () => {
   return (
-    <div className='App'>
+    <AppContainer>
       <EstilosGlobais />
-      <header className="App-header">
+      <HeaderEstilizado>
         <Logo />
         <ul>
           {itensHeader.map(
@@ -27,8 +49,8 @@ const App = () => {
             )
           )}
         </ul>
-      </header>
-    </div>
+      </HeaderEstilizado>
+    </AppContainer>
   )
 }
 
--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,14 +1,21 @@
-import './Logo.css'
 import logo from '../../images/logo.svg'
+import styled from 'styled-components'
+
+const LogoEstilizado = styled.div`
+  display: flex;
+  gap: 16px;
+  align-items: center;
+  font-size: 32px;
+`
 
 const Logo = () => {
   return (
-    <div className="logo">
+    <LogoEstilizado>
       <img src={logo} alt="Logo Alura Books" />
       <p>
         <strong>Alura</strong>Books
       </p>
-    </div>
+    </LogoEstilizado>
   )
 }
 
