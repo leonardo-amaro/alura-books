139633b375a5b179229a92a9331b4b2ad7e5eca4
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2025-03-31 11:08:34 -0300

    criando componente Pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,20 @@
 import styled from 'styled-components'
 import EstilosGlobais from './components/EstilosGlobais'
-import Logo from './components/Logo'
-import perfil from './images/perfil.svg'
-import sacola from './images/sacola.svg'
-
-const itensHeader = ['Categorias', 'Favoritos', 'Minha estante']
-const iconesHeader = [perfil, sacola]
+import Header from './components/Header'
+import Pesquisa from './components/Pesquisa'
 
 const AppContainer = styled.div`
   min-width: 100vw;
   min-height: 100vh;
   background-image: linear-gradient(90deg, #002F52 35%, #326589);
 `
-const HeaderEstilizado = styled.header`
-  background-color: #f1f1f1;
-  padding: 32px 16px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  ul {
-    display: flex;
-    gap: 16px;
-  }
-  ul li {
-    list-style-type: none;
-    cursor: pointer;
-    font-weight: 500;
-  }
-`
 
 const App = () => {
   return (
     <AppContainer>
       <EstilosGlobais />
-      <HeaderEstilizado>
-        <Logo />
-        <ul>
-          {itensHeader.map(
-            (item, index) => <li key={index}>{item}</li>
-          )}
-        </ul>
-        <ul>
-          {iconesHeader.map(
-            (icone, index) => (
-              <li key={index}>
-                <img src={icone} alt="Icone" />
-              </li>
-            )
-          )}
-        </ul>
-      </HeaderEstilizado>
+      <Header />
+      <Pesquisa />
     </AppContainer>
   )
 }
---
+++ src/components/Pesquisa/index.jsx
@@ -0,0 +1,15 @@
+const Pesquisa = () => {
+  return (
+    <section>
+      <h1>
+        Já sabe por onde começar?
+      </h1>
+      <p>
+        Encontre um livro em nossa estante.
+      </p>
+      <input type="text" />
+    </section>
+  )
+}
+
+export default Pesquisa
